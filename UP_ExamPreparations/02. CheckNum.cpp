3eb4da434232bfbee6d425852cc0aabcdce83d2f
aGatovski <145013465+aGatovski@users.noreply.github.com>
2024-02-04 14:18:45 +0200

    Create 02. CheckNum.cpp

---
+++ UP_ExamPreparations/02. CheckNum.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+void swap(int& a, int& b) {
+	int c = b;
+	b = a;
+	a = c;
+}
+
+unsigned numberLength(long N) {
+
+	unsigned length = 0;
+	while (N != 0)
+	{
+		N /= 10;
+		length++;
+	}
+
+	return length;
+
+}
+
+int checkNum(long n, unsigned m , unsigned l) {
+
+	unsigned numbLen = numberLength(n);
+	unsigned length = numbLen;
+	if (m > numbLen || l > numbLen) {
+		return -1;
+	}
+
+	int* array = new int[numbLen];
+
+	while (n !=  0)
+	{
+		
+		array[numbLen-1] = (n % 10);
+		n /= 10;
+		numbLen--;
+	}
+
+	
+	swap(array[m], array[l]);
+
+	
+
+	for (size_t i = 0; i < length; i++)
+	{
+		n = (n * 10) + array[i];
+			
+	}
+
+	return n%4==0 ;
+	
+}
+
+
+int main() {
+
+	long N;
+	std::cin >> N;
+	
+	unsigned m, l;
+	std::cin >> m >> l;
+
+	std::cout << checkNum(N, m, l);
+
+	
+	
+
+}
